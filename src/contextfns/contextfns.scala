aad6d2692b7a1885e8ff0ccc4e1bc60b4826c207
Jon Pretty <jon.pretty@propensive.com>
2022-12-06 18:17:59 +0100

    Add the "homework" I went through at the end of the lesson

--- src/contextfns/contextfns.scala
+++ src/contextfns/contextfns.scala
@@ -1,3 +1,17 @@
+/*
+Convert the Env type to an opaque type on Map[String, String]
+Create a method on Env called in, whose only parameter is a context function providing a “safe” 
+Env instance
+An Env which we know contains PATH and PWD should combine a set of types like Path and Pwd somehow
+in its type parameter. Decide how, and choose what variance Env’s type parameter should have to
+accommodate this
+Provide a method called env which can be called only inside the in block which takes a type
+parameter (no value parameters) to extract the appropriate variable from the environment
+Make sure the env method cannot be called unsafely
+Check the commented-out examples to see what should compile successfully or not.
+*/
+
+
 package contextfns
 
 import collection.JavaConverters.*
@@ -11,18 +25,29 @@ case class VarName[T](name: String)
 given VarName[Path]("PATH")
 given VarName[Pwd]("PWD")
 
-object Env:
-  def apply(): Env[Nothing] = Env(System.getenv.asScala.to(Map))
+object OpaqueEnv:
+  opaque type Env[-T] = Map[String, String]
+  object Env:
+    def apply(): Env[Nothing] = System.getenv.asScala.to(Map)
 
-case class Env[Values](map: Map[String, String]):
-  def guarantee[T](using varName: VarName[T]): Env[Values] =
-    if !map.contains(varName.name) then throw Exception("Missing value") else Env[Values](map)
+  extension [T](env: Env[T])
+    def in(fn: Env[T] ?=> Unit): Unit = ???
 
+    def get(key: String): String = env(key)
+  
+    def guarantee[V](using varName: VarName[V]): Env[T | V] =
+      if !env.contains(varName.name) then throw Exception("Missing value") else env
 
-/*
+import OpaqueEnv.Env
+
+def env[V: VarName](using Env[V]): String = summon[Env[V]].get(summon[VarName[V]].name)
+
 // This should compile
 val eg1 =
-  Env().guarantee[Path].guarantee[Pwd].in {
+  val e1: Env[Nothing] = Env()
+  val e2: Env[Path] = e1.guarantee[Path]
+  val e3: Env[Pwd | Path] = e2.guarantee[Pwd]
+  e3.in {
     println(env[Path])
     println(env[Pwd])
   }
@@ -30,16 +55,15 @@ val eg1 =
 def pwdAsFile(using Env[Pwd]): java.io.File =
   java.io.File(env[Pwd])
 
-// This should also compile
+// // This should also compile
 val eg2 =
   Env().guarantee[Pwd].in {
     println(pwdAsFile)
   }
 
-// This should not compile
+// // This should not compile
 val eg3 =
   Env().guarantee[Path].in {
     println(pwdAsFile)
   }
 
-*/
