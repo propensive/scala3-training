c6d9284d5dc1afa6f92e89e1e8742c0f06c0bba9
Jon Pretty <jon.pretty@propensive.com>
2022-12-07 14:37:34 +0100

    Include completed top-level definitions example

--- src/toplevel/main.scala
+++ src/toplevel/main.scala
@@ -1,5 +1,16 @@
 package greeting
 
+import scala.util.*
+
 object greet:
   def main(args: Array[String]): Unit =
     println("Hello world")
+
+
+enum Day:
+  case Mon, Tue, Wed, Thu, Fri, Sat, Sun
+
+given CommandLineParser.FromString[Day] = str => Day.fromOrdinal(str.toInt)
+
+@main
+def run(day: Day): Unit = println(day.toString)
