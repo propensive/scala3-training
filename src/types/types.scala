845c92c4f5fae1e5b515bca3a564334b86c172df
Jon Pretty <jon.pretty@propensive.com>
2022-12-07 14:38:00 +0100

    Completed union/intersection types answers

--- src/types/types.scala
+++ src/types/types.scala
@@ -10,23 +10,22 @@ object Checks:
   type B = Int
   type C = Exception
 
-/*
-  summon[           Any ?:? Nothing           ] // 1
-  summon[             A ?:? Nothing           ] // 2
-  summon[             B ?:? Any               ] // 3
-  summon[             C ?:? Nothing           ] // 4
-  summon[       Nothing ?:? AnyRef            ] // 5
+  summon[           Any >:> Nothing           ] // 1
+  summon[             A >:> Nothing           ] // 2
+  summon[             B <:< Any               ] // 3
+  summon[             C >:> Nothing           ] // 4
+  summon[       Nothing <:< AnyRef            ] // 5
 
-  summon[       (A & B) ?:? (B & A)           ] // 6
-  summon[       (A & B) ?:? Nothing           ] // 7
-  summon[       (A | B) ?:? Any               ] // 8
-  summon[       (A & B) ?:? (A | B)           ] // 9
-  summon[       (A | C) ?:? (A | B | C)       ] // 10
-  summon[   (A & B & C) ?:? (A & B)           ] // 11
-  summon[ (A & (B | C)) ?:? (C & A)           ] // 12
-  summon[   (A & B & C) ?:? (C & B & C & A)   ] // 13
-  summon[ (B | (A & C)) ?:? ((A | B) & (C | B)) ] // 14
-*/
+  summon[       (A & B) =:= (B & A)           ] // 6
+  summon[       (A & B) >:> Nothing           ] // 7
+  summon[       (A | B) <:< Any               ] // 8
+  summon[       (A & B) <:< (A | B)           ] // 9
+  summon[       (A | C) <:< (A | B | C)       ] // 10
+  summon[   (A & B & C) <:< (A & B)           ] // 11
+  summon[ (A & (B | C)) >:> (C & A)           ] // 12
+  summon[   (A & B & C) =:= (C & B & C & A)   ] // 13
+  summon[ (B | (A & C)) =:= ((A | B) & (C | B)) ] // 14
+
 // Some simple type definitions
 @targetName("unknownRelation")
 infix type ?:?[L, R]
